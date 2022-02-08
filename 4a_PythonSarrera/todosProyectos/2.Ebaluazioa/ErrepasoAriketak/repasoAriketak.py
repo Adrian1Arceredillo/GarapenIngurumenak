9f78e7e1f7577f1ad51b189272d5842ba0d5b965
Adrian1Arceredillo <aarceredillo2@gmail.com>
2022-02-08 14:22:27 +0100

    .

--- 4a_PythonSarrera/todosProyectos/2.Ebaluazioa/ErrepasoAriketak/repasoAriketak.py
+++ 4a_PythonSarrera/todosProyectos/2.Ebaluazioa/ErrepasoAriketak/repasoAriketak.py
@@ -2,6 +2,7 @@
 #ejercicio1
 #https://www.lawebdelprogramador.com/codigo/Python/3053-Calcular-los-multiplos-de-3-y-5.html
 import random
+from datetime import datetime, date
 
 
 #ariketa 1
@@ -128,12 +129,51 @@ def mesDiaPerteneceAEstacion():
     print(numeroMes + 1)
 
 
+#ejercicio 9
+def fechaDecirEstacion():
+    hilabeteGuztiak = (
+        'Urtarrila', 'Otsaila', 'Martxoa', 'Apirila', 'Maiatza', 'Ekaina', 'Uztaila', 'Abuztua', 'Iraila', 'Urria',
+        'Azaroa',
+        'Abendua');
+
+    actualFecha = date.today()  #fecha de hoy
+
+    primaveraInicio = date(2020, 3, 20)
+    primaveraFinal = date(2020, 6, 21)
+    print(primaveraFinal)
+
+    inicioVerano = date(2020, 6, 21)
+    finalVerano = date(2020, 9, 23)
+
+    inicioOtoño = date(2020, 9, 23)
+    finalOtoño = date(2020, 12, 21)
+
+    inicioInvierno = date(2020, 12, 21)
+    finalInvierno = date(2021, 3, 20)
+
+    stringMes = input("Sartu hilabete baten izena (urtarrila, otsaila...): ").capitalize()
+    diaMes = int(input("Sartu hilabetearen egun bat: "))
+
+    numeroMes = hilabeteGuztiak.index(stringMes)
+
+    # p.e.: año = 2020 - ya que al usuario no se le pide el valor del año
+    fechaUsuario = date(2020, numeroMes + 1, diaMes)
+    print("Fecha del usuario: " + str(fechaUsuario))
+
+    if (fechaUsuario > actualFecha):
+        print("no")
+    else:
+        print("si")
+
+
 #ejercicio 10
 def tablaDeMultiplicar():
     zenbakia = int(input("Sartu zenbaki bat (1-10): "))
 
     for i in range (1, 11):
         print(str(zenbakia) + " x " + str(i) + " = " + str(zenbakia * i))
+
+
 
 
 
@@ -170,6 +210,22 @@ def darLaVueltaString():
 
 
 #ejercicio 14
+def numerosConTodosLosDigitosImpares():
+    num1 = 100
+    num2 = 400
+
+    fullImpar = False
+
+    for i in range(num1, num2):
+        for x in str(i):
+            if int(x) % 2 != 0:
+                fullImpar = True
+            else:
+                fullImpar = False
+                break;
+        if (fullImpar) == True:
+            print(i)
+
 
 #ejercicio 15
 def mediaDe3Numeros():
@@ -229,7 +285,26 @@ def menorElementoLista():
     print("El menor numero de la lista es: " + str(txikiena))
 
 
+#prueba recorrer dígitos de un número
+def recorrerNumero():
+    numeroPrueba = 511
+    fullImpar = False
+    for x in str(numeroPrueba):
+        if int(x) % 2 != 0:
+            fullImpar = True
+        else:
+            fullImpar = False
+            break;
+
+    if fullImpar == True:
+        print("El numero es full impar")
+    else:
+        print("No es del todo impar ")
+
+
 #############################################
+#recorrerNumero()   #prueba - recorrer digitos de un numero / caracteres de un string
+fechaDecirEstacion()    #prueba - hacer pruebas con fechas
 
 #multiplos5y7() #ejercicio 1
 #cambioCelsiusFahrenheit() #ejercicio 2
@@ -239,12 +314,14 @@ def menorElementoLista():
 #contarLetrasNumerosDeString() #ejercicio 6
 #patronDibujarLetraA()   #ejercicio 7
 #letraVocalOConsonante() #ejercicio 8
+
 #mesDiaPerteneceAEstacion() #ejercicio 9 - Galdetu
+
 #tablaDeMultiplicar()   #ejercicio 10
 #trianguloConNumeros()  #ejercicio 11
 #adivinarNumeroOculto() #ejercicio 12
 #darLaVueltaString()    #ejercicio 13
-                        #ejercicio 14 - Galdetu
+#numerosConTodosLosDigitosImpares()  #ejercicio 14 - Galdetu
 #mediaDe3Numeros()   #ejercicio 15
 #sumaTodosLosElementosLista()    #ejercicio 16
 #multiplicarElementosLista()     #ejercicio 17
